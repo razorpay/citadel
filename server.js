11efbc5d02635bc599181ec9c5a632ca17aad6e1
Siddhant Agarwal <siddhant.agarwal@razorpay.com>
2021-03-30 10:08:04 +0530

    chore: Refactor Move createRedirects to a separate file

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const pug = require('pug');
 const chokidar = require('chokidar').watch([]);
 const webpack = require('webpack');
 const stylus = require('stylus');
-const yaml = require('js-yaml');
 const webpackConfigBase = require('./webpack.config');
-const { exec, execSync } = require('child_process');
+const { execSync } = require('child_process');
+const createRedirects = require('./create-redirects');
 
 const cfs = require('./scripts/cfs');
 const atRules = require('./scripts/at-rules');
-const customHtml = require('./scripts/custom-html');
 const markdown = require('./scripts/md');
 const { initializePlugin, applyPlugin, cleanupPlugin } = require('./plugins');
 
@@ -198,22 +197,6 @@ function checkAllowed(key, config) {
     }
   });
   return allow;
-}
-
-async function createRedirects(config) {
-  const BASE_PATH = config.basePath + config.publicPath;
-  const DIST_DIR = config.dist;
-  const REDIRECTS_FILE_PATH = process.env.PWD + '/' + config.redirects;
-  const redirectContent = await fs.readFile(REDIRECTS_FILE_PATH);
-  const redirects = yaml.load(redirectContent);
-  await Promise.all(
-    Object.keys(redirects).map(async (origin) => {
-      const url = new URL(BASE_PATH + redirects[origin]);
-      const html = `<html><head><meta http-equiv="refresh" content="0;URL=${url}"/></head></html>`;
-      await exec(`mkdir -p ${DIST_DIR}/${origin}/`);
-      await fs.writeFile(`${DIST_DIR}/${origin}/index.html`, html);
-    })
-  );
 }
 
 module.exports = {
