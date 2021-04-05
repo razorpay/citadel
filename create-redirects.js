6f92876b0baf2583588f186519a513f20400dadf
Siddhant Agarwal <siddhant.agarwal@razorpay.com>
2021-04-05 14:03:35 +0530

    feat: Support multiple redirect files

--- create-redirects.js
+++ create-redirects.js
@@ -2,12 +2,27 @@ const yaml = require('js-yaml');
 const { execSync } = require('child_process');
 const fs = require('fs');
 
+function getRedirectPaths(config) {
+  if (config.redirects) {
+    if (Array.isArray(config.redirects)) {
+      return config.redirects;
+    }
+    return [config.redirects]
+  }
+  return []
+}
+
 function createRedirects(config) {
   const BASE_PATH = config.basePath + config.publicPath;
   const DIST_DIR = config.dist;
-  const REDIRECTS_FILE_PATH = process.env.PWD + '/' + config.redirects;
-  const redirectContent = fs.readFileSync(REDIRECTS_FILE_PATH);
-  const redirects = yaml.load(redirectContent);
+  const redictPaths = getRedirectPaths(config);
+  const REDIRECT_FILE_PATHS = config.redirects.map(redirect => process.env.PWD + '/' + redirect)
+  const yamlContent = REDIRECT_FILE_PATHS.reduce((acc, redirectFilePath) => {
+    const content = fs.readFileSync(redirectFilePath);
+    return acc + "\n" + content
+  }, "")
+  const redirects = yaml.load(yamlContent);
+  console.log(redirects)
   Object.keys(redirects).map((origin) => {
     const url = new URL(BASE_PATH + redirects[origin]);
     const html = `<html><head><meta http-equiv="refresh" content="0;URL=${url}"/></head></html>`;
