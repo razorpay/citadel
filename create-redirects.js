b75041477c482c35aab7c0dd6209b3fca86b98ab
Siddhant Agarwal <siddhant.agarwal@razorpay.com>
2021-04-05 14:05:39 +0530

    chore: Refactor

--- create-redirects.js
+++ create-redirects.js
@@ -7,28 +7,30 @@ function getRedirectPaths(config) {
     if (Array.isArray(config.redirects)) {
       return config.redirects;
     }
-    return [config.redirects]
+    return [config.redirects];
   }
-  return []
+  return [];
 }
 
 function createRedirects(config) {
   const BASE_PATH = config.basePath + config.publicPath;
   const DIST_DIR = config.dist;
   const redictPaths = getRedirectPaths(config);
-  const REDIRECT_FILE_PATHS = config.redirects.map(redirect => process.env.PWD + '/' + redirect)
-  const yamlContent = REDIRECT_FILE_PATHS.reduce((acc, redirectFilePath) => {
+  const redirectFilePaths = redictPaths.map(
+    (redirect) => process.env.PWD + '/' + redirect
+  );
+  const redirectList = redirectFilePaths.reduce((acc, redirectFilePath) => {
     const content = fs.readFileSync(redirectFilePath);
-    return acc + "\n" + content
-  }, "")
-  const redirects = yaml.load(yamlContent);
-  console.log(redirects)
+    return acc + '\n' + content;
+  }, '');
+  const redirects = yaml.load(redirectList);
+  console.log(redirects);
   Object.keys(redirects).map((origin) => {
     const url = new URL(BASE_PATH + redirects[origin]);
     const html = `<html><head><meta http-equiv="refresh" content="0;URL=${url}"/></head></html>`;
     execSync(`mkdir -p ${DIST_DIR}/${origin}/`);
     fs.writeFileSync(`${DIST_DIR}/${origin}/index.html`, html);
-  })
+  });
 }
 
 module.exports = createRedirects;
