17b719989002c48d0822f31ff770df32642ceeb1
Pranav Gupta <pronav@users.noreply.github.com>
2021-04-15 20:27:37 +0530

    place null check

--- create-redirects.js
+++ create-redirects.js
@@ -26,12 +26,14 @@ function createRedirects(config) {
     return acc + '\n' + content;
   }, '');
   const redirects = yaml.load(redirectList);
-  Object.keys(redirects).map((origin) => {
-    const url = new URL(BASE_PATH + redirects[origin]);
-    const html = `<html><head><meta http-equiv="refresh" content="0;URL=${url}"/></head></html>`;
-    execSync(`mkdir -p ${DIST_DIR}/${origin}/`);
-    fs.writeFileSync(`${DIST_DIR}/${origin}/index.html`, html);
-  });
+  if (redirects) {
+    Object.keys(redirects).map((origin) => {
+      const url = new URL(BASE_PATH + redirects[origin]);
+      const html = `<html><head><meta http-equiv="refresh" content="0;URL=${url}"/></head></html>`;
+      execSync(`mkdir -p ${DIST_DIR}/${origin}/`);
+      fs.writeFileSync(`${DIST_DIR}/${origin}/index.html`, html);
+    }); 
+  }
 }
 
 module.exports = createRedirects;
