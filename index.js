a0b85aba960a8aafb6547c70cf3e647307c1e4c4
Neeraj Kumar Singh <neeraj.singh@razorpay.com>
2021-12-06 04:41:14 +0530

    added extra param code for creating redirects separately

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 const glob = require('glob').sync;
 const argv = require('yargs-parser')(process.argv.slice(2));
 const { getFormattedDoc } = require('./build-helpers');
-const {
-  getDocumentsRoot,
-  getDocumentsGlob,
-} = require('./fs-helpers');
+const { getDocumentsRoot, getDocumentsGlob } = require('./fs-helpers');
+const createRedirects = require('./create-redirects');
 const { readConfig } = require('./read-config');
 const path = require('path');
 
 const isServer = argv._.indexOf('serve') !== -1;
 const isSearch = argv._.indexOf('build-search') !== -1;
+const shouldCreateRedirects = argv._.indexOf('create-redirect') !== -1;
+
 const { serve, build } = require('./server');
 const { buildSearch } = require('./build-search');
 
@@ -35,6 +35,10 @@ async function compile(config) {
   const allDocs = {};
   const getDoc = await getFormattedDoc({ allDocs, getPath });
 
+  if (shouldCreateRedirects) {
+    createRedirects(config);
+    return;
+  }
   if (isServer) {
     serve({
       config,
